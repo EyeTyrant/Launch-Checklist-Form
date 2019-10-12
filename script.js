48099f541093a4beb5c5ee7939b89f6a8dbf7651
EyeTyrant <1414jg@gmail.com>
2019-10-12 17:51:17 -0500

    validation and updating complete

--- script.js
+++ script.js
@@ -1,51 +1,83 @@
 // Write your JavaScript code here!
 
-
 function updateChecklist(e) {
    e.preventDefault();
    let pilot = document.querySelector("input[name=pilotName]").value;
    let copilot = document.querySelector("input[name=copilotName").value;
    let fuel = document.querySelector('input[name=fuelLevel').value;
    let cargo = document.querySelector('input[name=cargoMass').value;
-   let faulty = document.querySelector('#faultyItems');
+   // let faulty = document.querySelector('#faultyItems');
    let pilotStat = document.getElementById('pilotStatus');
    let copilotStat = document.querySelector('#copilotStatus');
    let fuelStat = document.querySelector('#fuelStatus');
    let cargoStat = document.querySelector('#cargoStatus');
    let status = document.querySelector('#launchStatus');
+   let notReadyMsg = `Shuttle not ready for launch`;
+   let readyMsg = `Shuttle ready for launch, LET'S LIGHT THIS CANDLE!`;
+   let ready = 0;
+   let charVal = /^[a-z\s]+$/ig;
+   let numVal = /^[0-9]+$/ig;
    
-   if (pilot === '' || copilot === '' || fuel === ''|| isNaN(fuel) || cargo === '' || isNaN(cargo)){
+   if (pilot.match(charVal)){
+      pilotStat.style.visibility = 'visible';
+      pilotStat.innerHTML = `${pilot}`;
+      ready += 1;
+   } else {
+      pilotStat.style.visibility = 'hidden';
+      pilot = '';
       e.preventDefault();
-      alert('Please enter valid information.');   
+      alert('Please enter valid pilot name.');
    }
-
-   pilotStat.style.visibility = 'visible';
-   pilotStat.innerHTML = `${pilot}`;
-   copilotStat.style.visibility = 'visible';
-   copilotStat.innerHTML = `${copilot}`;
    
-   if (fuel < 10000) {
-      fuelStat.style.visibility = 'visible';
-      fuelStat.innerHTML = `${fuel} liters of rum is not enough fuel for the journey`;
-      status.innerHTML = `Shuttle not ready for launch`;
-      status.style.color = 'red';
+   if (copilot.match(charVal)){
+      copilotStat.style.visibility = 'visible';
+      copilotStat.innerHTML = `${copilot}`;
+      ready += 1;
    } else {
-      status.innerHTML = `Shuttle is ready for launch`;
-      status.style.color = 'green';
+      copilotStat.style.visibility = 'hidden';
+      copilot = '';
+      e.preventDefault();
+      alert('Please enter valid co-pilot name.');
+   }
    
-   }
-   if (cargo > 10000) {
-      cargoStat.style.visibility = 'visible';
-      cargoStat.innerHTML = `${cargo} kilograms of tequila has too much mass for the journey`;
-      status.innerHTML = `Shuttle not ready for launch`;
-      status.style.color = 'red';
-   } else {
-      status.innerHTML = `Shuttle is ready for launch`;
-      status.style.color = 'green';
-   }
-
-
+   if (fuel.match(numVal)){
+      if (fuel < 10000){
+         fuelStat.innerHTML = `Sadly ${pilot}, ${fuel} liters of rum is not enough fuel for the journey.`;
+         fuelStat.style.visibility = 'hidden';
+      } else {
+         fuelStat.style.visibility = 'visible';
+         fuelStat.innerHTML = `${fuel} liters of rum is plenty for the journey ${pilot}.`;
+         ready += 1;
       }
+   } else {
+      fuelStat.style.visibility = 'hidden';
+      e.preventDefault();
+     alert('Please enter valid fuel information.');
+   }  
+
+   if (cargo.match(numVal)){
+      if (cargo > 10000) {
+         cargoStat.innerHTML = `Unfortunately ${copilot}, ${cargo} kilograms of tequila has too much mass for the journey.`;
+         cargoStat.style.visibility = 'hidden';
+      } else {
+         cargoStat.innerHTML = `${cargo} kilograms of tequila is just enough to make the trip ${copilot}`;
+         cargoStat.style.visibility = 'visible';
+         ready += 1;
+      } 
+   } else {
+      cargoStat.style.visibility = 'hidden';
+      e.preventDefault();
+      alert('Please enter valid cargo information.');
+   }
+   
+   if (ready === 4){
+      status.innerHTML = `${readyMsg}`;
+      status.style.color = 'green';
+   } else {
+      status.innerHTML = `${notReadyMsg}`;
+      status.style.color = 'red';
+   }
+}
 
 window.addEventListener("load", function() {
    let form = document.querySelector("form");
